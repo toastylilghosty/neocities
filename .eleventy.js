8c9f8ffa43281be02f4aaea6eea1bc4d5b766af1
Christina M <toastylilghostie@gmail.com>
2026-01-03 00:04:00 -0500

    feat: website first draft

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,5 @@
+import { globSync } from 'glob';
+
 export const config = {
     dir: {
         input: "dev",
@@ -6,7 +8,26 @@ export const config = {
 };
 
 export default function(eleventyConfig) {
+    const galleries = globSync(["public/img/**/*.*"]);
+
+    eleventyConfig.addCollection("galleries", function (collection) {
+        let items = galleries.map((x) => {
+            let paths = x.split("\\")
+            let name = paths[paths.length -1]
+            let gallery = paths[paths.length - 2]
+            return {
+                gallery: gallery,
+                path: "img/" + gallery + "/" + name,
+                name: name
+            }
+        })
+        console.log(items)
+        return items
+    });
+
     eleventyConfig.addPassthroughCopy("dev/style.css");
     eleventyConfig.addPassthroughCopy("dev/robots.txt");
     eleventyConfig.addPassthroughCopy("dev/img/*");
+    eleventyConfig.addPassthroughCopy("dev/img/**/*");
+    eleventyConfig.addGlobalData("permalink", "{{ page.filePathStem }}.html");
 };
