792ac8f2d77b642d44ecf62c23d410de2188db65
Christina M <toastylilghostie@gmail.com>
2026-01-11 16:37:44 -0500

    feat: autolinks

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,4 @@
+import { readFileSync } from 'fs';
 import { globSync } from 'glob';
 
 export const config = {
@@ -21,9 +22,30 @@ export default function(eleventyConfig) {
                 name: name
             }
         })
-        console.log(items)
         return items
     });
+
+    const links = readFileSync(("./dev/websites.csv"),  { encoding: 'utf8', flag: 'r' }, (err, data) => {
+        if (err) throw err;
+        console.log(data);
+        console.log("websites!")
+    });
+
+    eleventyConfig.addCollection("links", function (collection) {
+        //name, url, tag
+        let items = links.split("\n");
+
+        let linkCollection = items.map((x) => {
+            let item = x.split(",")
+            return {
+                name: item[0],
+                url: item[1],
+                tag: item[2]
+            }
+        })
+
+        return linkCollection
+    })
 
     eleventyConfig.addPassthroughCopy("dev/style.css");
     eleventyConfig.addPassthroughCopy("dev/robots.txt");
